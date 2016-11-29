b424ed0fa5a283e2509335675e618baca7f07c9b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-11-29 23:56:42 +0100

    Added post callback.

--- app/controllers/pollCreator.client.js
+++ app/controllers/pollCreator.client.js
@@ -5,19 +5,18 @@
 
 (function () {
 
-    var optionList = document.querySelector("#options-list");
-    var createButton = document.querySelector("#create-button");
-    var addButton = document.querySelector("#add-option-button");
-    var pollNameInput = document.querySelector("#poll-name-input");
+    var optionList      = document.querySelector("#options-list");
+    var createButton    = document.querySelector("#create-button");
+    var addButton       = document.querySelector("#add-option-button");
+    var pollNameInput   = document.querySelector("#poll-name-input");
     var optionNameInput = document.querySelector("#option-name-input");
+    var pollCreateMsg   = document.querySelector("#poll-create-message");
+
     var apiUrl = appUrl + '/polls/create';
-    //
-    //
 
     // Returns the option parameters for a poll
     var getOptParams = function() {
         var options = document.getElementsByClassName("poll-option");
-        //var res = "options=";
         var res = "";
         for (var i = 0; i < options.length; i++) {
             res += "options=" + options[i].innerHTML;
@@ -26,15 +25,27 @@
         return res;
     };
 
+    /** Send a new poll to the server using POST.*/
     createButton.addEventListener("click", function() {
-        var params = "";
+        var params    = "";
         var optParams = getOptParams();
-        var pollName = pollNameInput.value;
+        var pollName  = pollNameInput.value;
+
         params += "name=" + pollName + "&" + optParams;
         console.log("Params are: " + params);
-        ajaxFunctions.ajaxRequest('POST', apiUrl, function (data) {}, params);
+
+        // Callback called after POST finishes
+        var postCb = function(data) {
+            if (data) {
+                var json = JSON.parse(data);
+                pollCreateMsg.innerHTML = json.msg;
+            }
+        };
+
+        ajaxFunctions.ajaxRequest('POST', apiUrl, postCb, params);
     });
 
+    // When clicked, adds a new option to the Poll
     addButton.addEventListener("click", function() {
         console.log("Poll name is " + pollNameInput.value);
         var optElem = document.createElement("li");
