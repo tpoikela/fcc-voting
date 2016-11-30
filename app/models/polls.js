8eace5c7fb07161b14a312a27f91a5318e034b5a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-12-01 00:03:51 +0100

    Added unit tests for Poll model. Added some validation methods into the Poll schema.

--- app/models/polls.js
+++ app/models/polls.js
@@ -4,14 +4,47 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var nameValidator = function(v) {
+    return /^[^<>]+$/.test(v);
+};
+
+var arrayNameValidator = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (!nameValidator(arr[i])) return false;
+    }
+    return true;
+};
+
 var Poll = new Schema({
-	name: String,
+    name: {
+        type: String, 
+        required: [true, "Poll name required"],
+        validate: {
+            validator: nameValidator,
+            message: "Name cannot contain < or >.",
+        },
+    },
     options: {
-        names: [String],
+        names: {
+            type: [String],
+            validate: {
+                validator: arrayNameValidator,
+                message: "Opt name cannot contain < or >.",
+            },
+        },
         votes: [Number],
     },
+
+    // Contains info about creator and voters of the poll
     info: {
-        creator: String,
+        creator: {
+            type: String,
+            required: [true, "Each poll must have a creator."],
+            validate: {
+                validator: nameValidator,
+                message: "Creator name cannot contain < or >.",
+            },
+        },
         voters: [String],
     },
 });
---
+++ tests/polls.js
@@ -0,0 +1,36 @@
+
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const Poll = require("../app/models/polls.js");
+
+describe('Poll objects', function() {
+
+    it('should have a name', function() {
+        var poll = new Poll();
+        poll.name = {};
+        var error = poll.validateSync();
+        expect(error).to.not.equal(undefined);
+        expect(error.errors['name']).to.exist;
+        expect(error.errors['options']).to.not.exist;
+
+        poll.name = "<script>alert('xss');</script>";
+        error = poll.validateSync();
+        expect(error.errors['name']).to.exist;
+    });
+
+    it('should have voting options', function() {
+        var poll = new Poll();
+
+        poll.options.names = ["a", "b"];
+        var error = poll.validateSync();
+        expect(error.errors['options.names']).to.equal(undefined);
+        poll.options.names = ["<a>", "b"];
+        error = poll.validateSync();
+        expect(error.errors['options.names']).to.not.equal(undefined);
+        expect(error.errors['options.names'].message).to.equal("Opt name cannot contain < or >.");
+
+    });
+
+});
