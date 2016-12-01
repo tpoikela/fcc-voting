ddfd38e89491461221bec18634e615df9e87c837
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-12-01 14:10:58 +0100

    Added an array of Poll IDs.

--- app/models/users.js
+++ app/models/users.js
@@ -3,7 +3,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+/** A schema for an user in the database.*/
 var User = new Schema({
+
+    //username: {required: true, type: String},
+
+    // Bit tricky to support 2 usernames, change to one only?
     github: {
         id: String,
         displayName: String,
@@ -12,8 +19,9 @@ var User = new Schema({
     },
     local: {
         username: String,
-        password: String,
+        password: String, // TODO use bcrypt to handle this
     },
+    polls: [{type: ObjectId, ref: 'Poll' }],
 });
 
 module.exports = mongoose.model('User', User);
