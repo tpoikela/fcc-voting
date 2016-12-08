7fe1e52c6291b8cc2d1ab8a1346aa47659bffeea
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-12-08 22:47:30 +0100

    Added validation module and corresponding tests.

---
+++ app/common/validation.js
@@ -0,0 +1,31 @@
+
+
+module.exports = {
+
+    minPasswordLen: 1,
+
+    validateName: function(name) {
+        if (name === null) return false;
+        if (typeof name === "string") {
+            return /^[^<>]+$/.test(name);
+        }
+        return false;
+    },
+
+    validateNameArray: function(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (!this.validateName(arr[i])) return false;
+        }
+        return true;
+    },
+
+    validatePassword: function(pw) {
+        if (pw === null) return false;
+        if (typeof pw === "string") {
+            return pw.length >= this.minPasswordLen;
+        }
+        return false;
+
+    },
+
+};
---
+++ tests/validation.js
@@ -0,0 +1,48 @@
+/**
+ * Test file for app/common/validation.js
+ */
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const V = require("../app/common/validation");
+
+describe('Validation module', function() {
+
+    it('ensures names dont have < or >', function() {
+        var result = V.validateName("<script>alert('xss');</script>");
+        expect(result).to.equal(false);
+
+        result = V.validateName("Tuomas Poikela");
+        expect(result).to.equal(true);
+
+        result = V.validateName(null);
+        expect(result).to.equal(false);
+    });
+
+    it('verifies also full arrays of names', function() {
+
+        var arrOk = ["aaafa", "asda12312sa", "dsa?&#Q@"];
+        var result = V.validateNameArray(arrOk);
+        expect(result).to.equal(true);
+
+        arrNotOk = ["aa<a>fa", "asda12312sa", "dsa?&#Q@"];
+        result = V.validateNameArray(arrNotOk);
+        expect(result).to.equal(false);
+
+        var nullArr = ["Tttt", "xXxax", null];
+        result = V.validateNameArray(nullArr);
+        expect(result).to.equal(false);
+
+    });
+
+    it('validates passwords', function() {
+        var result = V.validatePassword('');
+        expect(result).to.equal(false);
+
+        result = V.validatePassword('goodPassWord');
+        expect(result).to.equal(true);
+
+    });
+
+});
