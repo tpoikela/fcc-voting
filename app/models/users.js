9cc23ab26008463948c0022fb5b036919766d03c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-12-08 22:48:59 +0100

    Name validation moved to common module. Added common username.

--- app/models/users.js
+++ app/models/users.js
@@ -1,27 +1,43 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Validation = require('../common/validation.js');
+var nameValidator = Validation.validateName;
+var arrayNameValidator = Validation.validateNameArray;
 
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 /** A schema for an user in the database.*/
 var User = new Schema({
 
-    //username: {required: true, type: String},
+    // Used to access the information in database
+    username: {
+        required: true,
+        type: String,
+        validate: {
+            validator: nameValidator,
+            message: "Name cannot contain < or >",
+        },
+    },
 
-    // Bit tricky to support 2 usernames, change to one only?
+    // Used for github auth only
     github: {
         id: String,
         displayName: String,
         username: String,
-        publicRepos: Number
     },
+
+    // Used for local user and password auth
     local: {
         username: String,
         password: String, // TODO use bcrypt to handle this
     },
+
+    // Each poll is referenced by its object ID
     polls: [{type: ObjectId, ref: 'Poll' }],
+
 });
 
 module.exports = mongoose.model('User', User);
