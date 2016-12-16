dd9dd42910b0ca788393eac6368a814b07a79e1a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-12-16 11:14:52 +0100

    Added unit test for getPoll.

--- tests/pollController.serverTest.js
+++ tests/pollController.serverTest.js
@@ -17,12 +17,16 @@ describe('How pollController on server side works', function() {
 
     beforeEach(function() {
         req = mocks.createRequest();
+
         res = mocks.createResponse();
+        //res.json = sinon.stub(); // Stub out res.json()
+        sinon.stub(res, "json");
+
         ctrl = new PollController(process.cwd());
     });
 
     afterEach(function() {
-
+        res.json.restore();
     });
 
     it('should return all polls as a result', function() {
@@ -33,6 +37,7 @@ describe('How pollController on server side works', function() {
         ctrl.getPolls(req, res);
 
         expect(res.statusCode).to.equal(200);
+        expect(res.json.calledWith(expected)).to.be.true;
 
     });
 
@@ -45,6 +50,31 @@ describe('How pollController on server side works', function() {
         ctrl.getPollAsJSON(req, res);
 
         expect(res.statusCode).to.equal(200);
+        expect(res.json.calledWith(expected)).to.be.true;
+
+    });
+
+    it('Adds one poll into the database', function() {
+        var expMsg = {msg: "New poll has been created."};
+        var user = {_id: 1234, username: "TestUser", polls: []};
+        var userFindOne = sinon.stub(User, "findOne");
+        userFindOne.yields(null, user);
+
+        var userUpdate = sinon.stub(User, "update");
+        userUpdate.yields(null);
+
+        var pollSaveMock = sinon.mock(Poll);
+        Poll.prototype.save = function(cb) {cb(null);};
+
+        req.body.name = "TestPoll";
+        req.body.options = ["A", "B", "C", "D"];
+
+        req.user = user;
+
+        Promise.all([ctrl.addPoll(req, res)]).then(function() {
+            sinon.assert.calledOnce(res.json);
+            expect(res.json.calledWith(expMsg));
+        });
 
     });
 
