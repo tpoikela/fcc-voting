060940ebda52009cf060aad82630cf28e69b6b5b
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-12-19 22:35:16 +0100

    Added sinon-mongoose to verify findOne-populate-exec.

--- tests/userController.serverTest.js
+++ tests/userController.serverTest.js
@@ -1,7 +1,11 @@
 
 const expect = require("chai").expect;
+
 const sinon  = require("sinon");
+require("sinon-mongoose");
+
 const mocks = require("node-mocks-http");
+
 
 const Fact = require("./factory");
 
@@ -19,40 +23,31 @@ describe('How userController on server-side works', function() {
     var res = null;
     var ctrl = null;
 
-    var userFindOne = null;
-    var userSave = null;
-
-    var userPopulate = null;
-    var userExec = null;
+    var findOne = null;
+    var save = null;
 
     beforeEach(function() {
         // Mock req/res for all unit tests
         req = Fact.getMockedReq();
         res = Fact.getMockedRes();
-
         ctrl = new UserController(process.cwd());
 
-        userFindOne = sinon.stub(User, "findOne");
-        userSave = sinon.stub(User.prototype, "save");
-
-        userPopulate = sinon.stub(User, "populate");
-        userExec = sinon.stub(User, "exec");
-
+        findOne = sinon.stub(User, "findOne");
+        save = sinon.stub(User.prototype, "save");
     });
 
     afterEach(function() {
         Fact.restoreRes(res);
-        userFindOne.restore();
-        userSave.restore();
-        userPopulate.restore();
-        userExec.restore();
+        findOne.restore();
+        save.restore();
     });
 
-    it('should add a local user into the database', function(done) {
+    it('should add a local user into the database', sinon.test(function(done) {
         var user = Fact.createUser();
-        userFindOne.yields(null, null);
-        userSave.yields(null);
         req.body = {username: user.username, password: "xxx"};
+
+        findOne.yields(null, null);
+        save.yields(null);
 
         Promise.all([ctrl.addLocalUser(req, res)]).then(
             function() {
@@ -67,13 +62,13 @@ describe('How userController on server-side works', function() {
                 done();
             }
         );
-    });
+    }));
 
     it('should not overwrite existing user', function(done) {
         var user = Fact.createUser();
-        userFindOne.yields(null, user);
-        userSave.yields(null);
         req.body = {username: user.username, password: "xxx"};
+
+        findOne.yields(null, {username: user.username});
 
         Promise.all([ctrl.addLocalUser(req, res)]).then(
             function() {
@@ -95,7 +90,11 @@ describe('How userController on server-side works', function() {
         var user = Fact.createUser();
         req.user = user;
 
-        userExec.yields(user);
+        findOne.restore();
+
+        sinon.mock(User).expects("findOne")
+            .chain("populate").withArgs("polls")
+            .chain("exec").yields(null, user);
 
         Promise.all([ctrl.getUser(req, res)]).then(function() {
             sinon.assert.calledOnce(res.json);
@@ -105,6 +104,5 @@ describe('How userController on server-side works', function() {
         });
 
     });
-
 
 });
